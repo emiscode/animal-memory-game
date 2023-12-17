023bf9587fc11259ab8d5e281bc537ca964431bd
Emilio Scodeler <emilio.scodeler@solides.com.br>
2023-12-17 15:55:27 -0300

    add sound effects

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,14 +2,53 @@ import React, { useState, useEffect } from 'react';
 import Board from './Board';
 import { shuffle, generateAnimalPairs } from '../helpers';
 import { CardProps } from './Card';
+import backgroundSound from '../sounds/background-music.mp3';
+import flipSound from '../sounds/card-flip-2.mp3';
+import correctSound from '../sounds/correct.mp3';
+import ohNoSound from '../sounds/oh-no.mp3';
+import gameOverSound from '../sounds/game-over.mp3';
+import useSound from 'use-sound';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeUp } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
 
 const Game: React.FC = () => {
+  const [playGameOverSound] = useSound(gameOverSound);
+  const [playFlipSound] = useSound(flipSound);
+  const [playOhNoSound] = useSound(ohNoSound);
+  const [playCorrectSound] = useSound(correctSound);
   const [cards, setCards] = useState<CardProps[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [canFlip, setCanFlip] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [flippedCards, setFlippedCards] = useState<number[]>([]);
   const [matchedCards, setMatchedCards] = useState<number[]>([]);
   const [unmatchedCards, setUnmatchedCards] = useState<number[]>([]);
+
+  const [restart, setRestart] = useState<boolean>(true);
+  const [volume, setVolume] = useState<number>(0.3);
+  const [playBackgroundSound, { stop, sound }] = useSound(backgroundSound, {
+    loop: true,
+    volume,
+  });
+
+  const restartGame = () => {
+    // Reset state variables
+    setFlippedCards([]);
+    setMatchedCards([]);
+
+    // Shuffle cards
+    const shuffledCards = shuffle(cards);
+    setCards(shuffledCards);
+
+    stop();
+    playBackgroundSound();
+  };
+
+  useEffect(() => {
+    if (sound) {
+      sound.volume(volume);
+    }
+  }, [sound, volume]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -24,9 +63,28 @@ const Game: React.FC = () => {
       });
   }, []);
 
+  useEffect(() => {
+    playBackgroundSound();
+    return () => {
+      stop();
+    };
+  }, [playBackgroundSound, stop]);
+
+  useEffect(() => {
+    if (
+      (flippedCards.length > 1 && flippedCards.length === cards.length) ||
+      (matchedCards.length > 1 && matchedCards.length === cards.length)
+    ) {
+      stop();
+      playGameOverSound();
+      setRestart(true);
+    }
+  }, [flippedCards, matchedCards, cards, stop, playGameOverSound, restart]);
+
   if (isLoading) {
     return (
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center mt-4">
+        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-orange-800"></div>
         <div className="text-xl font-bold text-orange-800 ml-2">
           Iniciando...
         </div>
@@ -36,6 +94,8 @@ const Game: React.FC = () => {
 
   const handleCardClick = (id: number) => {
     if (!canFlip) return;
+
+    playFlipSound();
 
     // Check if the card is already flipped or matched
     if (flippedCards.includes(id) || matchedCards.includes(id)) {
@@ -58,10 +118,12 @@ const Game: React.FC = () => {
         firstCard.animal.name === secondCard.animal.name
       ) {
         // Match found
+        playCorrectSound();
         setMatchedCards([...matchedCards, ...newFlippedCards]);
         // Reset flipped cards
         setFlippedCards([]);
       } else {
+        playOhNoSound();
         setCanFlip(false);
         // No match, set unmatched cards
         setUnmatchedCards(newFlippedCards);
@@ -77,8 +139,36 @@ const Game: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center py-8">
-      {' '}
+    <div className="flex flex-col items-center py-8">
+      <div className="mb-4 flex items-center">
+        <FontAwesomeIcon icon={faVolumeUp} className="mr-2" />
+        <input
+          className="mr-2"
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={(e) => setVolume(parseFloat(e.target.value))}
+        />
+        <button onClick={(event) => stop()} className="ml-2 mr-2">
+          <FontAwesomeIcon icon={faStop} />
+        </button>
+        <button
+          onClick={(event) => playBackgroundSound()}
+          className="ml-2 mr-2"
+        >
+          <FontAwesomeIcon icon={faPlay} />
+        </button>
+        {restart && (
+          <button
+            onClick={(event) => restartGame()}
+            className="ml-2 mr-2 bg-gradient-to-r from-yellow-500 to-orange-500 hover:from-orange-500 hover:to-yellow-500 px-4 py-2 rounded text-gray-700 shadow-lg font-bold"
+          >
+            Reiniciar
+          </button>
+        )}
+      </div>
       <Board
         cards={cards.map((card) => ({
           ...card,
